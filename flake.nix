c0929b7809e3163e76b910f3d221964d5a98f19e
Ross MacLeod <ross@Lumen>
2023-08-18 00:26:50 +0000

    playground support

--- flake.nix
+++ flake.nix
@@ -6,7 +6,14 @@
 
   outputs = inputs: inputs.flake-utils.lib.eachDefaultSystem (system:
     let
-      pkgs = import inputs.nixpkgs { inherit system; };
+      pkgs = import inputs.nixpkgs {
+        inherit system;
+        overlays = [
+          (self: super: {
+            tree-sitter = super.tree-sitter.override (_: { webUISupport = true; });
+          })
+        ];
+      };
     in {
       devShells.default = pkgs.mkShell {
         packages = [
